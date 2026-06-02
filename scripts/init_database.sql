053d1d3fa036a387ab78bdc2e1e98e15f2906c88
morgankim <morgan022497@gmail.com>
2026-06-02 14:43:23 -0700

    bronze, silve, and gold schemas created.

---
+++ scripts/init_database.sql
@@ -0,0 +1,48 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict EaJy5uJGJ8Zkx9cbs5AftoFeEMgvqeaE5NVgssXfYVsXY7jNgl2NYlj26xHJQIA
+
+-- Dumped from database version 18.4 (365f1e4)
+-- Dumped by pg_dump version 18.4
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: bronze; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA bronze;
+
+
+--
+-- Name: gold; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA gold;
+
+
+--
+-- Name: silver; Type: SCHEMA; Schema: -; Owner: -
+--
+
+CREATE SCHEMA silver;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict EaJy5uJGJ8Zkx9cbs5AftoFeEMgvqeaE5NVgssXfYVsXY7jNgl2NYlj26xHJQIA
+
